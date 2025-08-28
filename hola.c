d8a3d0e71abdf0fe01688e4e58a95d8f2b6d486d
tralalero tralala <nahuel.disanti@gmail.com>
2025-08-28 19:07:04 -0300

    makefile

---
+++ hola.c
@@ -0,0 +1,6 @@
+
+#include <stdio.h> // Incluye la biblioteca stdio.h
+int main() { // main es el punto de entrada
+printf("Hola Orga!\n"); // imprime en pantalla
+return 0; // devuelve un 0 al SO
+}
